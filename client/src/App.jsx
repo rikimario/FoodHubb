ea2bb29fa65367a82d5689e846347f45029a495b
rikimario <mario_martinov@abv.bg>
2023-11-05 23:12:52 +0200

    import header footer and nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer.jsx"
+import Header from "./components/Header.jsx"
 import NavBar from "./components/NavBar.jsx"
+
 
 function App() {
 
   return (
     <>
       <NavBar />
+
+      <Header />
+
+      <Footer />
     </>
   )
 }
