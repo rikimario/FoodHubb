831c908da6d76d4db2b6feb92ed16f35a9c93ff1
rikimario <mario_martinov@abv.bg>
2023-11-06 00:58:49 +0200

    add main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
 import NavBar from "./components/NavBar.jsx"
+// import './assets/styles.css'
 
 
 function App() {
 
   return (
-    <>
+    <div>
       <NavBar />
 
-      <Header />
+      <main className="flex-shrink-0">
+        <Header />
+      </main>
 
       <Footer />
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default App
